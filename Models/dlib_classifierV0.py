d70b607c3aabaa2e58ed9f4770a1f80432f6037e
ArunSeetharaman <38047697+ArunSeetharaman@users.noreply.github.com>
2018-04-14 12:28:49 -0400

    Add files via upload

--- Models/dlib_classifierV0.py
+++ Models/dlib_classifierV0.py
@@ -9,22 +9,29 @@ import scipy.io as sio
 import numpy as np
 
 #define paramters of model
-input_shape = 128
+input_dim = 128
 num_classes = 530
 batch_size = 64
-epochs = 10
+epochs = 35
 
 #import data
-
+x_train = sio.loadmat('DataWithMeanDlib/train_features.mat')['train_features']
+x_test = sio.loadmat('DataWithMeanDlib/test_features.mat')['test_features']
+y_train = sio.loadmat('Class/training_class.mat')['training_class']
+y_test = sio.loadmat('Class/test_class.mat')['test_class']
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
 
 #define model
 model = Sequential()
-model.add(Dense(512, kernel_initializer = RandomNormal(mean=0.0,stddev=.01), activation = 'relu', input_shape=input_shape))
+model.add(Dense(512, kernel_initializer = RandomNormal(mean=0.0,stddev=.01), activation = 'relu', input_dim=input_dim))
 model.add(Dense(1024,kernel_initializer = RandomNormal(mean=0.0,stddev=.01), activation = 'relu'))
+model.add(Dropout(.5))
 model.add(Dense(num_classes, kernel_initializer = RandomNormal(mean=0.0,stddev=.01), activation = 'softmax'))
 
 #compile, fit and evaluate model
 model.compile(loss = categorical_crossentropy, optimizer = 'adam', metrics = ['accuracy'])
+#model.compile(loss = categorical_crossentropy, optimizer = SGD(lr = .01, momentum = .9), metrics = ['accuracy'])
 
 model.save('dlib_classifierV0_untrained.h5')
 
