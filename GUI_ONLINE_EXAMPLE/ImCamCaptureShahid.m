fd7b3aa75989152c50299769ffbf23b37d58fec4
gkordell <38047552+gkordell@users.noreply.github.com>
2018-04-17 13:43:53 -0400

    Add files via upload

--- GUI_ONLINE_EXAMPLE/ImCamCaptureShahid.m
+++ GUI_ONLINE_EXAMPLE/ImCamCaptureShahid.m
@@ -7,7 +7,7 @@ function varargout = ImCamCaptureShahid(varargin)
 
 % Edit the above text to modify the response to help ImCamCaptureShahid
 
-% Last Modified by GUIDE v2.5 16-May-2013 21:03:08
+% Last Modified by GUIDE v2.5 17-Apr-2018 13:40:42
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -144,3 +144,26 @@ function figure1_CloseRequestFcn(hObject, eventdata, handles)
     delete (instrfind) 
 % Hint: delete(hObject) closes the figure
 delete(hObject);
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
