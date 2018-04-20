b23ffb7fa807e58f5ba110661bba61c39c21369b
cdefran <chrisdefrancisci@gmail.com>
2018-04-19 23:14:32 -0400

    added label adjustments

--- Online Learning/online_learning_V0.py
+++ Online Learning/online_learning_V0.py
@@ -9,7 +9,7 @@
 # 3. Re-train the keras model on the new dataset with the new face added from step 4
 
 
-# from keras.utils import to_categorical  
+from keras.utils import to_categorical  
 from keras.models import Sequential, load_model
 # from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 # from keras.optimizers import *
@@ -28,11 +28,22 @@ aug_num = 1
 ## STEP 1 ---------------------------------------------------------------------
 #import new image, get features
 # images, x = augment_and_extract_features('train_base_online/image/1144d9ab29825e4b5f29f0497b78a593f627c0ea.jpg', aug_num)
-x = np.random.rand(1,128)
-label = np.array([0]) # ? How to get label? parameter?
+x_train = np.random.rand(128,1)
+y_train = np.array([0]) # ? How to get label? parameter?
 # get testing data
 x_test = sio.loadmat('../Transfer Learning/dlib_features/test_features.mat')['test_features']
 y_test = sio.loadmat('../Transfer Learning/Class/test_class.mat')['test_class']
+
+current_num_classes = np.max(y_test) + 1   # this assumes that all classes are present in y
+
+x_train = np.transpose(x_train)
+x_test = np.transpose(x_test)
+
+y_train = to_categorical(y_train, current_num_classes)
+y_test = to_categorical(y_test, current_num_classes)
+
+y_train = np.squeeze(y_train)
+y_test = np.squeeze(y_test)
 
 ## STEP 2 ---------------------------------------------------------------------
 #load pretrained model
@@ -42,8 +53,8 @@ model = load_model('../Models/dlib_classifierV0_untrained.h5')
 #retrain model with new face
 
 history = model.fit(
-	x, 
-	label,
+	x_train, 
+	y_train,
 	batch_size = aug_num,
 	epochs = 3,
 	validation_data = (x_test, y_test))
